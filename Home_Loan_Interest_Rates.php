0a8afb21111f4006023aed6564896390e66f0549
Shiva Tiwari <shivatiwari757@gmail.com>
2019-10-21 20:55:36 +0530

    Update Home_Loan_Interest_Rates.php

--- Home_Loan_Interest_Rates.php
+++ Home_Loan_Interest_Rates.php
@@ -23,7 +23,7 @@
 
    <?php
 
-require_once("header.php");
+require_once("Header.php");
 ?>
 
     <br>
@@ -149,7 +149,7 @@ Your home loan eligibility or how much loan you are eligible for is also calcula
 
 <?php
 
-require_once("footer.php");
+require_once("Footer.php");
 ?>
 
 </body>
